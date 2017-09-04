32c600990087f08484c834bb09824758efdc6ab7
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-04 16:52:48 -0700

    Add page to create a new post and react-router links

--- post/src/index.js
+++ post/src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter> 
-      <Route path="/" component={PostsIndex}  />
-      <Route path="" component={}  />
-      <Route path="" component={}  />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/posts/new" component={PostsNew} />
+        <Route path="/" component={PostsIndex} />
+      </Switch>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
